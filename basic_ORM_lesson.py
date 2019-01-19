61de9bedbff98a1632ffc6edab15441625ea3466
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2019-01-19 19:16:42 +0000

    Adding objects and relations.

---
+++ basic_ORM_lesson.py
@@ -0,0 +1,29 @@
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base = declarative_base()
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='addresses')
+    def __repr__(self):
+        return f"<Address(email_address={self.email_address})>"
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+    addresses = relationship('Address', order_by=Address.id, back_populates='user')
+    def __repr__(self):
+        return f"<User(name={self.name}, fullname={self.fullname}, password={self.password})>
+
