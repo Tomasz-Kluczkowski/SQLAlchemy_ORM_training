4a94ec70f3f6095285cc01ec43758c626e7fb287
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2019-01-19 23:08:20 +0000

    Create jupyter kernel for training and base notebook file.

--- basic_ORM_lesson.py
+++ basic_ORM_lesson.py
@@ -13,6 +13,7 @@ class Address(Base):
     email_address = Column(String, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship('User', back_populates='addresses')
+
     def __repr__(self):
         return f"<Address(email_address={self.email_address})>"
 
@@ -24,6 +25,7 @@ class User(Base):
     fullname = Column(String)
     password = Column(String)
     addresses = relationship('Address', order_by=Address.id, back_populates='user')
+
     def __repr__(self):
-        return f"<User(name={self.name}, fullname={self.fullname}, password={self.password})>
+        return f"<User(name={self.name}, fullname={self.fullname}, password={self.password})>"
 
